a59ee77de42ebf773d4b0837b884783dca63f22f
SAAGAR <stimalsena155@gmail.com>
2023-05-12 12:16:49 +0545

    routing added

--- MagicView/Controllers/VillaAPIController.cs
+++ MagicView/Controllers/VillaAPIController.cs
@@ -9,29 +9,52 @@ namespace MagicView.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public ActionResult <IEnumerable<VillaDto>> GetVillas()
+        public ActionResult<IEnumerable<VillaDto>> GetVillas()
         {
             return Ok((VillaStore.villaList));
         }
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name ="GetVilla")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
 
         public ActionResult<VillaDto> GetVillas(int id)
         {
-            if( id==0)
+            if (id == 0)
             {
                 return BadRequest();
             }
             var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
-            if(villa == null)
+            if (villa == null)
             {
                 return NotFound();
             }
             return Ok(villa);
-/*
-            return Ok(VillaStore.villaList.FirstOrDefault(u=>u.Id==id));*/
+            /*
+                        return Ok(VillaStore.villaList.FirstOrDefault(u=>u.Id==id));*/
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDto> CreateVilla([FromBody] VillaDto villaDto)
+        {
+            if (villaDto == null)
+            {
+                return BadRequest(villaDto);
+            }
+            if (villaDto.Id>0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            villaDto.Id = VillaStore.villaList.OrderByDescending(u=> u.Id).FirstOrDefault().Id+1;
+            VillaStore.villaList.Add(villaDto);
+
+            return CreatedAtRoute("GetVilla", new { id = villaDto.Id}, villaDto);
+
         }
     }
+
+
 }
--- MagicView/Models/Dto/VillaDto.cs
+++ MagicView/Models/Dto/VillaDto.cs
@@ -1,8 +1,12 @@
-﻿namespace MagicView.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicView.Models.Dto
 {
     public class VillaDto
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(20)]
         public string Name { get; set; }
         public DateTime CreatedDate { get; set; }
     }
